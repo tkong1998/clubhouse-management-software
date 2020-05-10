24a98dccd7892c6d42f84743da0d27942110f119
tkong1998 <tkong@connect.ust.hk>
2020-05-10 12:43:01 +0800

    reformat

--- controller/Main.java
+++ controller/Main.java
@@ -14,29 +14,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	/** Holds the various scenes to switch between */
 	private static Map<String, Scene> scenes = new HashMap<>();
 	
 	@Override
 	public void start(Stage stage) {
 		
-		// Create and store all scenes up front
 		scenes.put("SIGNIN", new SigninView(stage).getScene());
 		scenes.put("NAVIGATION", new NavigationView(stage).getScene());
 		scenes.put("MANAGER_NAVIGATION", new ManagerNavigotionView(stage).getScene());
         scenes.put("RESERVATION", new ReservationView(stage).getScene());
         scenes.put("USAGE", new ViewUsageView(stage).getScene());
-		// scenes.put(SceneName.SCENE2, new ViewTwo(stage).getScene());
-		// scenes.put(SceneName.SCENE3, new ViewThree(stage).getScene());
 		
-		// Start with the main scene
 		stage.setScene(scenes.get("SIGNIN"));
 		stage.setTitle("HandShake Club House");
 		stage.show();
 	}
 
-	/** Returns a Map of the scenes by {@link SceneName} */
 	public static Map<String, Scene> getScenes() {
 		return scenes;
 	}
--- view/ViewMaker.java
+++ view/ViewMaker.java
@@ -2,12 +2,6 @@ package view;
 import javafx.scene.Scene;
 
 public interface ViewMaker {
-	
-	/**
-	 * Build and return a scene for this view
-	 * 
-	 * @return the Scene for this view
-	 */
 	Scene getScene();
 }
 
